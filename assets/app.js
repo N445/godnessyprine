4840c23a81bf4b28926b90d1a80f3bd4aaa9f3bc
Robin PARISOT <rparisot@nvl.fr>
2022-08-05 12:00:15 +0000

    add content

--- assets/app.js
+++ assets/app.js
@@ -10,3 +10,22 @@ import './styles/app.scss';
 
 // start the Stimulus application
 import './bootstrap';
+
+import * as bootstrap from 'bootstrap'
+
+
+let addToCartBtn = $('.main-cta');
+let gradiant = $('.gradiant');
+
+addToCartBtn.mousemove(function (e) {
+    const {
+              left: t,
+              width: n,
+              top: o,
+              height: i,
+          } = e.target.getBoundingClientRect(), r = (e.clientX - t) / n * 100, s = (e.clientY - o) / i * 100;
+
+    gradiant.css('--mouse-x', String(r));
+    gradiant.css('--mouse-y', String(s));
+    gradiant.css('background-position', 'calc((100 - var(--mouse-x, 0)) * 1%) calc((100 - var(--mouse-y, 0)) * 1%)');
+})
--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -11,8 +11,35 @@ class DefaultController extends AbstractController
     #[Route('/', name: 'HOMEPAGE')]
     public function index(): Response
     {
-        return $this->render('default/index.html.twig', [
-            'controller_name' => 'DefaultController',
+        return $this->render('default/homepage.html.twig', [
+        ]);
+    }
+
+    #[Route('/viens-je-te-dis-tout', name: 'ABOUT')]
+    public function about(): Response
+    {
+        return $this->render('default/about.html.twig', [
+        ]);
+    }
+
+    #[Route('/contacte-les-deesses', name: 'CONTACT')]
+    public function contact(): Response
+    {
+        return $this->render('default/contact.html.twig', [
+        ]);
+    }
+
+    #[Route('/boutique', name: 'SHOP')]
+    public function shop(): Response
+    {
+        return $this->render('default/shop.html.twig', [
+        ]);
+    }
+
+    #[Route('/quelques-idees-partagees', name: 'IDEAS')]
+    public function ideas(): Response
+    {
+        return $this->render('default/ideas.html.twig', [
         ]);
     }
 }
